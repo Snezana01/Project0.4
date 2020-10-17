12881d38266ff701d69f9bc6a0bc6f62fadd3d8a
Snežana Nikonova <71488175+Snezana01@users.noreply.github.com>
2020-10-17 16:11:44 +0300

    Update struktura.h

--- struktura.h
+++ struktura.h
@@ -1,20 +1,6 @@
 #ifndef STRUKTURA_H_INCLUDED
 #define STRUKTURA_H_INCLUDED
 
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip>
-#include <bits/stdc++.h>
-#include <random>
-#include <fstream>
-#include <algorithm>
-#include <iterator>
-#include <cstdlib>
-
-using std::cout;
-using std::cin;
 using std::string;
 using std::vector;
 
